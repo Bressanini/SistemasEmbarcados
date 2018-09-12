8c1e2f7820d4a9e75cd456b7fc252c246bffe3c6
Letícia Marchi <gabriel.bressa@hotmail.com>
2018-09-12 18:22:12 -0300

    Compilador Python na distrubuição

---
+++ buildroot/custom-scripts/server.py
@@ -0,0 +1,34 @@
+import SimpleHTTPServer
+import SocketServer
+import os.path, sys
+import datetime
+
+#Formating string. The %s will be substituted by system data.
+html = "<!DOCTYPE html>  \
+		<html> \
+			<body> \
+				<h1>System Information</h1> \
+				<p>Local Data: %s</p> \
+			</body> \
+		</html>"
+
+#Handle de http requests.
+class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+	#Build the html file for every connection.
+	def makeindex(self):
+	    tofile = html % datetime.datetime.now().time()
+		f = open("index.html", "w")
+		f.write(tofile)
+		f.close()
+   return
+	
+	#Method http GET.	
+	def do_GET(self):
+		self.makeindex()
+		self.path = '/index.html'
+		return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+#Start the webserver.
+Handler = MyRequestHandler
+server = SocketServer.TCPServer(('0.0.0.0', 8080), Handler)
+server.serve_forever()
